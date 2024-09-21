58505b2d9e5aefa9158bafc4d2ff8dc95e4fff95
UMER <umerkhan830863@gmail.com>
2024-09-22 02:28:15 +0530

    add one new file

--- 7_area_triangle.py
+++ 7_area_triangle.py
@@ -4,6 +4,6 @@ and using map funtion we can take input in one line and assign it into two varia
 spilt() function basically doing split the input in the basis of apacing.
 split()  splits the input string at spaces by default (you can specify other delimiters if needed).
 """
-height,breadth = map(float,input("Enter height and breagth by spacing between them for calculating the area of triangle:").split())
-area_of_a_triangle = 0.5*height*breadth
+height,base = map(float,input("Enter height and base by spacing between them for calculating the area of triangle:").split())
+area_of_a_triangle = 0.5*height*base
 print(area_of_a_triangle)
