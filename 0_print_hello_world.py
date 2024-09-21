8efc7a804b16629dee21301e47b2ffc507c37334
UMER <umerkhan830863@gmail.com>
2024-09-22 01:52:42 +0530

    add one new file

---
+++ 0_print_hello_world.py
@@ -0,0 +1,14 @@
+
+print("Hello","world!!!",sep="->",end="",flush=True)
+
+"""
+It takes five arguments or parameters are as follows:
+print(object(s), sep=separator, end=end, file=file, flush=flush)
+
+1) object(s)	Any object, and as many as you like. Will be converted to string before printed
+2) sep='separator'	Optional. Specify how to separate the objects, if there is more than one. Default is ' '
+3) end='end'	Optional. Specify what to print at the end. Default is '\n' (line feed)
+4) file	Optional. An object with a write method. Default is sys.stdout
+5) flush	Optional. A Boolean, specifying if the output is flushed (True) or buffered (False). Default is False
+"""
+
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 <h1>This is a some basic programs in python.<br>If you want to add something you can.</h1>
+<h3>0. Python program to print hello world!!!.</h3>
 <h3>1. Python program to find sum of natural numbers.</h3>
 <h3>2. Python program to display powers of 2 using anonymous function.</h3>
 <h3>3. Python program to find numbers divisible by another number.</h3>
